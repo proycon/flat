12e0d9711142eeda1a034870fad84d3dfb913f9f
Maarten van Gompel <proycon@anaproy.nl>
2014-01-20 15:59:55 +0100

    update

--- urls.py
+++ urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'flat.views.home', name='home'),
+    url(r'^login/?$', 'flat.views.login', name='home'),
     # url(r'^flat/', include('flat.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
