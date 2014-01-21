d3e40472e90b1c2d41facd97aa6b84c766a8a014
Maarten van Gompel <proycon@anaproy.nl>
2014-01-21 12:55:41 +0100

    update

--- modes/viewer/views.py
+++ modes/viewer/views.py
@@ -1,8 +1,11 @@
-# Create your views here.
+from __future__ import print_function, unicode_literals, division, absolute_import
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 import flat.comm as comm
 import flat.settings as settings
 
 def view(request, docid):
+    data = comm.get(request,'/getdoc/%NS/' + docid)
     pass
 
 
