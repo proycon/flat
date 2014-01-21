ac1512ebba50d368bf087e274e5f4bafb5f23c53
Maarten van Gompel <proycon@anaproy.nl>
2014-01-21 13:04:11 +0100

    update

--- modes/viewer/views.py
+++ modes/viewer/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 import flat.comm as comm
 import flat.settings as settings
+import json
 
+@login_required
 def view(request, docid):
-    data = comm.get(request, '/getdoc/%NS%/' + docid + '/')
-    pass
+    doc = comm.get(request, '/getdoc/%NS%/' + docid + '/')
+    return render(request, 'viewer.html', {'dochtml': dochtml, 'docannotations': json.dumps(doc.annotations), 'loggedin': request.user.is_authenticated(), 'username': request.user.username})
 
 
