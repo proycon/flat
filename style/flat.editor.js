da4089401a2b02d69254a54c646290fd79849ca8
Maarten van Gompel <proycon@anaproy.nl>
2014-01-22 13:19:33 +0100

    update

--- style/flat.editor.js
+++ style/flat.editor.js
@@ -1,9 +1,41 @@
 editoropen = false;
 
 
+function showeditor(element) {
+    if ((element) && ($(element).hasClass(view))) {
+        if ((element.id)  && (annotations[element.id])) {            
+            s = "";
+            Object.keys(annotations[element.id]).forEach(function(annotationid){
+                annotation = annotations[element.id][annotationid];
+                if (viewannotations[annotation.type+"/" + annotation.set]) {
+                    if (annotationtypenames[annotation.type]) {
+                        label = annotationtypenames[annotation.type];
+                    } else {
+                        label = annotation.type;
+                    }
+                    if (annotation.set) {
+                        setname = annotation.set;
+                    } else {
+                        setname = "";
+                    }
+                    s = s + "<tr><th>" + label + "<br /><span class=\"setname\">" + setname + "</span></th><td><input value=\"" + annotation.class + "\"/></td></tr>";
+                }
+                 
+            });
+            if (s) {
+                s = "<div id=\"id\">" + element.id + "</div><table>"  + s + "</table>";
+                $('#editor div.body').html(s);
+                $('#editor').css({'display': 'block', 'top':mouseY+ 20, 'left':mouseX-100} );
+                $('#editor').show();    
+                editoropen = true;
+            }
+        }
+    }
+}
+
 function editor_onclick(element) {
     //open editor
-    editoropen = true;
+    showeditor(element);
 }
 
 function editor_onmouseenter(element) {
