245d6cfa4a20348d8162f7f7db471481d325daa3
Maarten van Gompel <proycon@anaproy.nl>
2014-01-24 22:11:45 +0100

    fix

--- modes/structureeditor/urls.py
+++ modes/structureeditor/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     url(r'^(?P<namespace>[\w\-\.]+)/(?P<docid>[\w\-\.]+)/?$', 'flat.modes.structureeditor.views.view', name='view'),
-    url(r'^(?P<namespace>[\w\-\.]+)/(?P<docid>[\w\-\.]+)/annotate/?$', 'flat.modes.structureeditor.views.annotate', name='annotate'),
+    #url(r'^(?P<namespace>[\w\-\.]+)/(?P<docid>[\w\-\.]+)/annotate/?$', 'flat.modes.structureeditor.views.annotate', name='annotate'),
 
 )
