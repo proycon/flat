62591739c8042b1e3cbbdad8e9b8594e408d4437
Maarten van Gompel <proycon@anaproy.nl>
2014-02-10 10:19:11 +0100

    update

--- modes/editor/urls.py
+++ modes/editor/urls.py
@@ -1,6 +1,5 @@
 #from django.conf.urls import patterns, include, url
 from django.conf.urls.defaults import *
-from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
--- modes/viewer/urls.py
+++ modes/viewer/urls.py
@@ -1,5 +1,5 @@
 #from django.conf.urls import patterns, include, url
-from django.conf.urls import * #patterns, include, url
+from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     # Examples:
