3a5dabe279021cb4b97122d40d8ee2033cdd8aa2
Maarten van Gompel <proycon@anaproy.nl>
2014-02-10 11:55:04 +0100

    fix

--- settings.py
+++ settings.py
@@ -229,7 +229,7 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'flat.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'flat.wsgi.application'
