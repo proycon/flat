5412f891535a65d076499fd6802697b6724222d1
Maarten van Gompel <proycon@anaproy.nl>
2014-02-10 12:13:09 +0100

    fix

--- modes/structureeditor/urls.py
+++ modes/structureeditor/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import patterns, include, url
+#from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     # Examples:
