4295d62c548d307ac8510549a66670d03aa65643
Maarten van Gompel <proycon@anaproy.nl>
2014-02-28 17:19:14 +0100

    fix

--- modes/editor/urls.py
+++ modes/editor/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     url(r'^(?P<namespace>[\w\-\.]+)/(?P<docid>[\w\-\.]+)/?$', 'flat.modes.editor.views.view', name='view'),
     url(r'^(?P<namespace>[\w\-\.]+)/(?P<docid>[\w\-\.]+)/annotate/?$', 'flat.modes.editor.views.annotate', name='annotate'),
     url(r'^(?P<namespace>[\w\-\.]+)/(?P<docid>[\w\-\.]+)/declare/?$', 'flat.modes.editor.views.declare', name='declare'),
-    url(r'^(?P<namespace>[\w\-\.]+)/(?P<docid>[\w\-\.]+)/history/?$', 'flat.modes.editor.views.history', name='history')
+    url(r'^(?P<namespace>[\w\-\.]+)/(?P<docid>[\w\-\.]+)/history/?$', 'flat.modes.editor.views.history', name='history'),
     url(r'^(?P<namespace>[\w\-\.]+)/(?P<docid>[\w\-\.]+)/revert/(?P<commithash>)/?$', 'flat.modes.editor.views.revert', name='revert')
 )
