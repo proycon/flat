c35af308d3af52e830b0a34d9e890959ba37088e
Maarten van Gompel <proycon@anaproy.nl>
2014-08-21 17:04:33 +0200

    fix

--- style/testflat.js
+++ style/testflat.js
@@ -33,7 +33,7 @@ function testinit(name, assert) {
 
 
 function testtext(elementselector, reference, message="") {
-    if (message == "") message = "Testing whether of " + elementselector + " is \"" + message + "\"";
+    if (message == "") message = "Testing whether of " + elementselector + " is \"" + reference + "\"";
     globalassert.equal($(valid(elementselector)).text().trim(), reference, message);
 }
 
