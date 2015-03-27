a0c7720d15d233904be2b9d95dddf3bcbce89359
Maarten van Gompel (proycon) <proycon@anaproy.nl>
2015-03-27 10:51:45 +0100

    added ALLOWED_HOSTS in custom config

--- config/rusettings.py
+++ config/rusettings.py
@@ -244,7 +244,7 @@ CONFIGURATIONS = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','flat.science.ru.nl','applejack.science.ru.nl','spitfire','applejack']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
