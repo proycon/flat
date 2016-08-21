c57190a8fcef15b416adb5d986f1162a8ec10bf3
Maarten van Gompel <proycon@anaproy.nl>
2016-08-21 13:31:18 +0200

    added input fields for adding new metadata (issue #39)

--- flat/style/flat.metadata.js
+++ flat/style/flat.metadata.js
@@ -11,6 +11,21 @@ function metadata_oninit() {
         s = s + "<tr><td class=\"key\"><input id=\"metakey" + i + "\" value=\"" + key + "\" /></td><td class=\"value\"><input id=\"metavalue" + i + "\" value=\"" + metadata[key] + "\" /></td></tr>\n";
         i++;
     });
+    newfields = 0;
+    s = s + "<tr id=\"metadataplaceholder\"></tr>";
     s = s + "</table>";
+    s = s + "<div class=\"buttons\"><button id=\"metadatasubmit\" onclick=\"metadata_submit\">Save changes</button> <button onclick=\"metadata_addinput()\">+</button></div>";
     $('#metadata').html(s);
+    metadata_addinput();
+}
+
+function metadata_addinput() {
+    var i = metadata.length + newfields;
+    newfields++; 
+    var s = "<tr><td class=\"key\"><input id=\"metakey" + i + "\" value=\"\" /></td><td class=\"value\"><input id=\"metavalue" + i + "\" value=\"\" /></td></tr>\n";
+    s = s + "<tr id=\"metadataplaceholder\"></tr>";
+    $('#metadataplaceholder')[0].outerHTML = s;
+}
+
+function metadata_submit() {
 }
