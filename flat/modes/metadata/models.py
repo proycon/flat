639bdba46e732b9728f11d7e5038b986b759dec1
Maarten van Gompel <proycon@anaproy.nl>
2016-09-25 14:37:53 +0200

    Add metadata index model (for issue #55)

---
+++ flat/modes/metadata/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class MetadataIndex(models.Model):
+    namespace = models.CharField(max_length=255, db_index=True)
+    docid = models.CharField(max_length=255, db_index=True)
+    key = models.CharField(max_length=150)
+    value = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "Metadata Index"
+        verbose_name_plural = "Metadata Index"
