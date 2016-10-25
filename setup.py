0eb532aacf012e2f2a323d9a482fffa916ed2ee3
Maarten van Gompel <proycon@anaproy.nl>
2016-10-25 11:27:44 +0200

    Better non-conflicting highlight colours for search results

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ else:
 
 setup(
     name = "FoLiA-Linguistic-Annotation-Tool",
-    version = "0.6.1", #Also change in flat/__init__.py !!
+    version = "0.6.2", #Also change in flat/__init__.py !!
     author = "Maarten van Gompel",
     author_email = "proycon@anaproy.nl",
     description = ("FLAT is a web-based linguistic annotation environment based around the FoLiA format (https://proycon.github.io/folia), a rich XML-based format for linguistic annotation. Flat allows users to view annotated FoLiA documents and enrich these documents with new annotations, a wide variety of linguistic annotation types is supported through the FoLiA paradigm."),
