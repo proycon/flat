3e9fe638d870845c92fd37526d85a333ec3c1ed3
Maarten van Gompel <proycon@anaproy.nl>
2017-11-01 13:24:57 +0100

    attempted fix #124

--- flat/style/flat.editor.js
+++ flat/style/flat.editor.js
@@ -1258,15 +1258,15 @@ function gather_changes() {
             editdata[i].text = $('#editfield' + i + 'text').val().trim();
             editdata[i].changed = true;
         }
-        if ((editdata[i].editform == 'correction') && (!editdata[i].changed) && ($('#editform' + i + 'correctionclass').val().trim() != editdata[i].oldcorrectionclass)) {
+        if ((editdata[i].editform == 'correction') && (!editdata[i].changed) && (editdata[i].oldcorrectionclass) && ($('#editform' + i + 'correctionclass').val().trim() != editdata[i].oldcorrectionclass)) {
             //edit of correction class only, affects existing correction
             editdata[i].correctionclasschanged = true;
             editdata[i].correctionclass = $('#editform' + i + 'correctionclass').val().trim();
             editdata[i].correctionset = $('#editformcorrectionset').val().trim();
-            if ((!editdata[i].correctionclass) && (editdata[i].oldcorrectionclass == "")) {
+            /*if ((!editdata[i].correctionclass) && (editdata[i].oldcorrectionclass == "")) {
                 throw "Error (" + i + "): Annotation " + editdata[i].type + " was changed and submitted as correction, but no correction class was entered";
                 //if oldcorrectionclass is set we have an explicit removal of a correction
-            }
+            }*/
             if ($('#editfield' + i) && ($('#editfield' + i).val() == editdata[i].class) && ($('#editfield' + i).val() != 'undefined') ) {
                 editdata[i].oldclass = editdata[i].class; //will remain equal
                 editdata[i].class = $('#editfield' + i).val().trim();
@@ -1517,7 +1517,11 @@ function build_queries(addtoqueue) {
                     }
                 } else {
                     action = "EDIT";
-                    query += "EDIT correction OF " + editdata[i].correctionset + " WHERE class = \"" + escape_fql_value(editdata[i].oldcorrectionclass) + "\" WITH class \"" + escape_fql_value(editdata[i].correctionclass)  + "\" annotator \"" + username + "\" annotatortype \"manual\" datetime now confidence " + editdata[i].confidence; //might not be specific enough? use ID instead
+                    query += "EDIT correction OF " + editdata[i].correctionset + " "
+                    if (editdata[i].oldcorrectionclass !== "") {
+                        query += "WHERE class = \"" + escape_fql_value(editdata[i].oldcorrectionclass) + "\" "
+                    }
+                    query += "WITH class \"" + escape_fql_value(editdata[i].correctionclass)  + "\" annotator \"" + username + "\" annotatortype \"manual\" datetime now confidence " + editdata[i].confidence; //might not be specific enough? use ID instead
                     returntype = "ancestor-focus";
                     //set target expression
                     if (editdata[i].targets.length > 0) {
