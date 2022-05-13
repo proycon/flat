8aafcc57f85adf3cb50863b49417aad33075b67b
Maarten van Gompel <proycon@anaproy.nl>
2022-05-13 23:33:29 +0200

    set ALLOWED_HOSTS for new container #182

--- settings.py
+++ settings.py
@@ -379,7 +379,7 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ "localhost", "127.0.0.1", environ.get("FLAT_DOMAIN", "flat.yourdomain.org") ]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
