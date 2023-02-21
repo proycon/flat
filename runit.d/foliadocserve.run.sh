3b14b8146681fa1fe89bb3af870e115cfdeadae2
Maarten van Gompel <proycon@anaproy.nl>
2023-02-21 13:36:26 +0100

    container: output foliadocserve log to stdout instead of stderr so docker logs catches it #179

--- runit.d/foliadocserve.run.sh
+++ runit.d/foliadocserve.run.sh
@@ -6,6 +6,7 @@ echo "Starting foliadocserve..." >&2
 mkdir -p "${FLAT_DOCROOT:-/data/flat.docroot}"
 chown ${UWSGI_UID:-100}:${UWSGI_GID:-100} "${FLAT_DOCROOT:-/data/flat.docroot}"
 chmod g+ws "${FLAT_DOCROOT:-/data/flat.docroot}"
-#set very permissive stderr
+#set very permissive stdout/stderr
+chmod a+rwx /proc/self/fd/1
 chmod a+rwx /proc/self/fd/2
-sudo -u nginx foliadocserve -d ${FLAT_DOCROOT:-/data/flat.docroot} --log /dev/stderr --git --gitshare false --expirationtime 120 -p 8080
+sudo -u nginx foliadocserve -d ${FLAT_DOCROOT:-/data/flat.docroot} --log ${FOLIADOCSERVE_LOG:-/dev/stdout} --git --gitshare false --expirationtime 120 -p 8080
